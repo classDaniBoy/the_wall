a823b722c8a5559a68946398a2f62eb43d1ae477
Daniel <magnottafernando@gmail.com>
2019-04-28 14:24:58 -0300

    added profile

---
+++ profile.php
@@ -0,0 +1,40 @@
+<?php 
+	include("includes/header.php");
+ ?>
+
+ 	<style type="text/css">
+	 	.wrapper {
+	 		margin-left: 0px;
+			padding-left: 0px;
+	 	}
+
+ 	</style>
+	
+ 	<div class="profile_left">
+ 		<img src="assets/images/profile_pics/defaults/head_emerald.png">
+
+ 		<div class="profile_info">
+ 			<p>Posts: 480</p>
+ 			<p>Likes: 8000</p>
+ 			<p>Friends: 423</p>
+ 		</div>
+
+ 		<form action="Lorem ipsum" method="POST">
+ 		</form>
+
+ 	</div>
+
+
+	<div class="main_column column">
+	Lorem Ipsum
+		
+
+
+	</div>
+
+
+
+
+	</div>
+</body>
+</html>
