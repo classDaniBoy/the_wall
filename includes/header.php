38c6e2e51b9c26822249ca5f518a6cff74eae9f3
Daniel <magnottafernando@gmail.com>
2019-04-28 13:47:04 -0300

    Implemented basic header

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,40 @@
 <html>
 <head>
 	<title>Bienvenidos a The Wall</title>
+	<!-- JAVASCRIPT -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="assets/js/bootstrap.js"></script>
+	
+	<!-- CSS -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
 </head>
 <body>
+	<div class="top_bar" >
+		<div class="logo">
+			<a href="index.php"> The Wall</a>
+		</div>
+		<nav>
+			<a href="#">
+				Lorem Ipsum
+			</a>
+			<a href="#">
+				<i class="fas fa-home fa-lg"></i>
+			</a>
+			<a href="#">
+				<i class="far fa-envelope fa-lg"></i>
+			</a>
+			<a href="#">
+				<i class="fas fa-user-cog fa-lg"></i>
+			</a>
+			<a href="#">
+				<i class="far fa-bell fa-lg"></i>
+			</a>
+			<a href="#">
+				<i class="fas fa-sign-out-alt fa-lg"></i>
+			</a>
+		</nav>
+	</div>
+
