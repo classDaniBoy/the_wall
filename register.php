65f62302483a61398269bf5845e17407f6d927ec
Daniel <magnottafernando@gmail.com>
2019-05-04 18:55:44 -0300

    finished the validations baaaaabyyyyy

--- register.php
+++ register.php
@@ -20,7 +20,7 @@
 			<br>
 			<div id="first">
 
-				<form action="register.php" method="POST">
+				<form name="logForm" action="feed.php" method="POST" onsubmit = "return log_validate();">
 					<input type="email" name="log_email" placeholder="Email" value="" required>
 					<br>
 
@@ -37,7 +37,7 @@
 
 			<div id="second">
 
-				<form enctype="multipart/form-data" action="register.php" method="POST">
+				<form enctype="multipart/form-data" name="regForm" action="feed.php" onsubmit = "return reg_validate();" method="POST">
 					<input type="text" name="reg_fname" placeholder="Nombre" value="" required>
 					<br>
 					<input type="text" name="reg_lname" placeholder="Apellido" value="" required>
@@ -50,10 +50,8 @@
 					<br>
 					<input type="password" name="reg_password2" placeholder="Confirm Password" required>
 					<br>
-					<input type="password" name="reg_password2" placeholder="Confirm Password" required>
-					<br>
 					<p>Seleccionar imagen de perfil</p>
-					<input type="file" name="profile_pic" required>
+					<input type="file" name="profile_pic" >
 					<br>
 					<input type="submit" name="register_button" value="Registrarse">
 					<br>
@@ -63,6 +61,111 @@
 
 		</div>
 	</div>
+
+	<script type = "text/javascript">
+      function log_validate() {
+			var email = document.logForm.log_email.value;
+			var email_re = /(\w+\@\w+\.\w+$)$/;
+	  		var email_val = email_re.test(email);      
+			
+			var pass = document.logForm.log_password.value;
+			var pass_re = /(\w+)$/;
+	  		var pass_val = pass_re.test(pass);      
+	        
+	        if(email_val === false) {
+	            alert( "Please provide a valid email adress" );
+	            document.logForm.log_email.focus() ;
+	            return false;
+	         }
+	         console.log(pass);
+	         console.log(pass_re);
+	         console.log(pass_val);
+	        if( pass_val === false) {
+	            alert( "Please provide a valid password!" );
+	            document.logForm.log_password.focus() ;
+	            return false;
+	        }
+	        return true;
+      	}
+
+      	function reg_validate() {
+			var email = document.regForm.reg_email.value;
+			var email_re = /(\w+\@\w+\.\w+$)$/;
+	  		var email_val = email_re.test(email);   
+
+	  		var email2 = document.regForm.reg_email2.value;
+			var email_re2 = /(\w+\@\w+\.\w+$)$/;
+	  		var email_val2 = email_re2.test(email2);      
+			
+			var fname = document.regForm.reg_fname.value;
+			var fname_re = /(\w+)$/;
+	  		var fname_val = fname_re.test(fname);
+
+	  		var lname = document.regForm.reg_lname.value;
+			var lname_re = /(\w+)$/;
+	  		var lname_val = lname_re.test(lname);      
+
+			var pass = document.regForm.reg_password.value;
+			var pass_re = /(\w+)$/;
+	  		var pass_val = pass_re.test(pass); 
+
+	  		var pass2 = document.regForm.reg_password2.value;
+			var pass_re2 = /(\w+)$/;
+	  		var pass_val2 = pass_re2.test(pass2);      
+	        
+	        if (pass2 !== pass) {
+	        	alert( "Passwords don't match" );
+	            document.regForm.reg_password.focus() ;
+	            return false;
+	        }
+	        if (email !== email2) {
+	        	alert( "Email adresses don't match" );
+	            document.regForm.reg_email.focus() ;
+	            return false;
+	        }
+	        if(email_val === false) {
+	            alert( "Please provide a valid email adress" );
+	            document.regForm.reg_email.focus() ;
+	            return false;
+	         }
+	         if(email_val2 === false) {
+	            alert( "Please provide a valid email adress" );
+	            document.regForm.reg_email2.focus() ;
+	            return false;
+	         }
+	         if(fname_val === false) {
+	            alert( "Name is a mandatory field" );
+	            document.regForm.reg_fname.focus() ;
+	            return false;
+	         }
+	         if(lname_val === false) {
+	            alert( "Last Name is a mandatory field" );
+	            document.regForm.reg_lname.focus() ;
+	            return false;
+	         }
+	         if(pass_val === false) {
+	            alert( "Password is a mandatory field" );
+	            document.regForm.reg_password.focus() ;
+	            return false;
+	         }
+	         if(pass_val2 === false) {
+	            alert( "Password is a mandatory field" );
+	            document.regForm.reg_password2.focus() ;
+	            return false;
+	         }
+	         console.log(pass);
+	         console.log(pass_re);
+	         console.log(pass_val);
+	        if( pass_val === false) {
+	            alert( "Please provide a valid password!" );
+	            document.regForm.reg_password.focus() ;
+	            return false;
+	        }
+	        return true;
+      	}
+
+</script>
 <?php 
     include("includes/footer.php");
  ?>
+(\w+\@\w+\.\w+$)
