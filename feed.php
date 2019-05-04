5ce1c247568ad0e49a014f255662f6f6ae4fd4b5
Cesar <cesar_elcapo_91@hotmail.com>
2019-05-04 16:49:46 -0300

    Finishing feed for now

--- feed.php
+++ feed.php
@@ -3,6 +3,14 @@
  ?>
 
  <link rel="stylesheet" type="text/css" href="assets/css/feedstyle.css">
+
+ <style type="text/css">
+   body{
+    background-image: url("assets/images/backgrounds/desktop.jpg");
+   }
+  </style>
+
+
  <div class="wrapper">
     
   <div class="user_details column">
@@ -33,8 +41,11 @@
     <div class="posted_by">
       <a href="profile.php">Lorem ipsum</a>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tempor nisl in mi sodales, in vestibulum arcu laoreet.</p>
-      <form class="likear"action="feed.php" method="POST">
-        <input type="submit" name="like" id="like" value="Me gusta">
+      <form class="likear" action="feed.php" method="POST">
+        <input type="submit" class="comment_like" name=like_button value="Me gusta">
+        <div class="like_value">
+          <p>25 Likes</p>
+        </div>
       </form>
     </div>
   </div>
@@ -47,8 +58,11 @@
       <a href="profile.php">Lorem ipsum</a>
       <p>Donec cursus pharetra orci, vel venenatis sapien lobortis et.</p>
     </div>
-     <form class="likear"action="feed.php" method="POST">
-        <input type="submit" name="like" id="like" value="Me gusta">
+      <form class="likear" action="feed.php" method="POST">
+        <input type="submit" class="comment_like" name=like_button value="Me gusta">
+        <div class="like_value">
+          <p>47 Likes</p>
+        </div>
       </form>
   </div>
 
@@ -60,8 +74,11 @@
       <a href="profile.php">Lorem ipsum</a>
       <p>Phasellus lobortis pellentesque urna vel tincidunt. Curabitur dictum lorem vel cursus eleifend.</p>
     </div>
-     <form class="likear"action="feed.php" method="POST">
-        <input type="submit" name="like" id="like" value="Me gusta">
+     <form class="likear" action="feed.php" method="POST">
+        <input type="submit" class="comment_like" name=like_button value="Me gusta">
+        <div class="like_value">
+          <p>1 Likes</p>
+        </div>
       </form>
   </div>
 
@@ -73,10 +90,13 @@
       <a href="profile.php">Lorem ipsum</a>
       <p>Ut quis ipsum eu ligula rhoncus porta in ac felis.</p>
     </div>
-     <form class="likear"action="feed.php" method="POST">
-        <input type="submit" name="like" id="like" value="Me gusta">
+     <form class="likear" action="feed.php" method="POST">
+        <input type="submit" class="comment_like" name=like_button value="Me gusta">
+        <div class="like_value">
+          <p>225 Likes</p>
+        </div>
       </form>
-  </div>
+    </div>
 
     <div class="status_post column">
     <div class="status_post_profile_pic">
@@ -86,8 +106,11 @@
       <a href="profile.php">Lorem ipsum</a>
       <p>Vestibulum efficitur erat sit amet tellus ultricies, eu imperdiet nisl fringilla. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
     </div>
-     <form class="likear"action="feed.php" method="POST">
-        <input type="submit" name="like" id="like" value="Me gusta">
+       <form class="likear" action="feed.php" method="POST">
+        <input type="submit" class="comment_like" name=like_button value="Me gusta">
+        <div class="like_value">
+          <p>3 Likes</p>
+        </div>
       </form>
   </div>
 
@@ -99,8 +122,11 @@
       <a href="profile.php">Lorem ipsum</a>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tempor nisl in mi sodales, in vestibulum arcu laoreet.</p>
     </div>
-     <form class="likear"action="feed.php" method="POST">
-        <input type="submit" name="like" id="like" value="Me gusta">
+      <form class="likear" action="feed.php" method="POST">
+        <input type="submit" class="comment_like" name=like_button value="Me gusta">
+        <div class="like_value">
+          <p>0 Likes</p>
+        </div>
       </form>
   </div>
 
@@ -112,8 +138,11 @@
       <a href="profile.php">Lorem ipsum</a>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tempor nisl in mi sodales, in vestibulum arcu laoreet.</p>
     </div>
-     <form class="likear"action="feed.php" method="POST">
-        <input type="submit" name="like" id="like" value="Me gusta">
+    <form class="likear" action="feed.php" method="POST">
+        <input type="submit" class="comment_like" name=like_button value="Me gusta">
+        <div class="like_value">
+          <p>4 Likes</p>
+        </div>
       </form>
   </div>
 
@@ -125,8 +154,11 @@
       <a href="profile.php">Lorem ipsum</a>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tempor nisl in mi sodales, in vestibulum arcu laoreet.</p>
     </div>
-     <form class="likear"action="feed.php" method="POST">
-        <input type="submit" name="like" id="like" value="Me gusta">
+    <form class="likear" action="feed.php" method="POST">
+        <input type="submit" class="comment_like" name=like_button value="Me gusta">
+        <div class="like_value">
+          <p>12 Likes</p>
+        </div>
       </form>
   </div>
 
@@ -138,8 +170,11 @@
       <a href="profile.php">Lorem ipsum</a>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tempor nisl in mi sodales, in vestibulum arcu laoreet.</p>
     </div>
-     <form class="likear"action="feed.php" method="POST">
-        <input type="submit" name="like" id="like" value="Me gusta">
+    <form class="likear" action="feed.php" method="POST">
+        <input type="submit" class="comment_like" name=like_button value="Me gusta">
+        <div class="like_value">
+          <p>5 Likes</p>
+        </div>
       </form>
   </div>
 
@@ -151,8 +186,11 @@
       <a href="profile.php">Lorem ipsum</a>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tempor nisl in mi sodales, in vestibulum arcu laoreet.</p>
     </div>
-     <form class="likear"action="feed.php" method="POST">
-        <input type="submit" name="like" id="like" value="Me gusta">
+   <form class="likear" action="feed.php" method="POST">
+        <input type="submit" class="comment_like" name=like_button value="Me gusta">
+        <div class="like_value">
+          <p>2 Likes</p>
+        </div>
       </form>
   </div>
 
