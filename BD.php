9a0b4ebd18e01daf9b825b502898000b1cd382f5
Cesar <cesar_elcapo_91@hotmail.com>
2019-06-20 12:41:41 -0300

    Auto stash before merge of "master" and "origin/master"

--- BD.php
+++ BD.php
@@ -1,5 +1,5 @@
 <?php 
-	$mysqli = mysqli_connect("localhost","root","","mydb");
+	$mysqli = mysqli_connect("localhost","root","","usuarios");
 	if (mysqli_connect_errno($mysqli)) {
 		echo "fallo al conectar a MYSQL: " . mysqli_connect_error();
 	}
