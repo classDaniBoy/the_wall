c35258a4807f58c459c1d63ee6d4cafb959ecd89
Cesar <cesar_elcapo_91@hotmail.com>
2019-06-23 17:45:12 -0300

    finishing search

--- includes/header.php
+++ includes/header.php
@@ -23,7 +23,7 @@
 		</div>
 		<div class="search">
 			<form action="search.php" method="GET" name="search_form" >
-            	<input  type="text" onkeyup="getLiveSearchUsers(this.value, 1)" name="q" placeholder="Search" autocomplete="off" id="search_text_input"aria-label="Search">
+            	<input  type="text" onkeyup="getLiveSearchUsers(this.value,$_SESSION['id'])" name="q" placeholder="Search" autocomplete="off" id="search_text_input">
             	<div class="button_holder">
             		<img src="assets/images/glass.png">
             	</div>
